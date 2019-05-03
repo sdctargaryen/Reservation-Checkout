c2740df6cb4d3c60fa3a3945f05e1d623dcfaff5
Tracy Yu <tracyyu93@g.ucla.edu>
2019-05-02 19:45:54 -0700

    Remove morgan module

--- server/index.js
+++ server/index.js
@@ -1,14 +1,12 @@
 // var nr = require('newrelic');
 const express = require('express');
 const bodyParser = require('body-parser');
-const morgan = require('morgan');
 const path = require('path');
 const router = require('./router.js');
 const app = express();
 const port = process.argv[2] || 3003;
 
 app.use(bodyParser.json());
-app.use(morgan('dev'));
 
 app.use('/', express.static(path.join(__dirname, '../client/dist')));
 
